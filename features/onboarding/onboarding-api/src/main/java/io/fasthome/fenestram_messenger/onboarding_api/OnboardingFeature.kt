1e8406e4ec4ae6555ca7b1e2737a7454f8e54fa0
vsibi <jizis138@gmail.com>
2022-08-17 17:37:37 +0800

    GLOBAL-746 Модуль

---
+++ features/onboarding/onboarding-api/src/main/java/io/fasthome/fenestram_messenger/onboarding_api/OnboardingFeature.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by Viktor Solodskiy on 17.08.2022.
+ */
+package io.fasthome.fenestram_messenger.onboarding_api
+
+interface OnboardingFeature {
+}
---
+++ features/onboarding/onboarding-impl/src/main/java/io/fasthome/fenestram_messenger/onboarding_impl/OnboardingFeatureImpl.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by Viktor Solodskiy on 17.08.2022.
+ */
+package io.fasthome.fenestram_messenger.onboarding_impl
+
+import io.fasthome.fenestram_messenger.onboarding_api.OnboardingFeature
+
+class OnboardingFeatureImpl : OnboardingFeature {
+}
