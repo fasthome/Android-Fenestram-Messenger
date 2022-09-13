355fc70c0b48f01699697723120fc37398c38b4f
Dmitry <dvm101@yandex.ru>
2022-09-13 21:40:22 +0300

    GLOBAL-862 Исправлена ошибка с копированием аватарки у другого пользователя

--- common/core/src/main/java/io/fasthome/fenestram_messenger/core/ui/extensions/ImageViewExt.kt
+++ common/core/src/main/java/io/fasthome/fenestram_messenger/core/ui/extensions/ImageViewExt.kt
@@ -3,19 +3,24 @@
  */
 package io.fasthome.fenestram_messenger.core.ui.extensions
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import coil.load
 import coil.transform.CircleCropTransformation
+import io.fasthome.fenestram_messenger.core.R
 
 fun ImageView.loadCircle(url: String?, placeholderRes: Int? = null) {
-    if(url.isNullOrEmpty()) return
-    this.load(url) {
-        placeholderRes?.let {
-            placeholder(placeholderRes)
+    if (url.isNullOrEmpty())
+        placeholderRes?.let { this.load(placeholderRes) }
+    else
+        this.load(url) {
+            placeholderRes?.let {
+                placeholder(placeholderRes)
+            }
+            transformations(CircleCropTransformation())
         }
-        transformations(CircleCropTransformation())
-    }
 }
 
 fun ImageView.loadCircle(@DrawableRes imageRes: Int?, placeholderRes: Int? = null) {
