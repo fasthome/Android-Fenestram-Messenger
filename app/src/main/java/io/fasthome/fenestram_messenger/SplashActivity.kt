8f7d4919ab8172abbe455baa82a80dc94e26f974
vsibi <jizis138@gmail.com>
2022-09-15 17:31:47 +0800

    GLOBAL-954 Реализован сплеш скрин

---
+++ app/src/main/java/io/fasthome/fenestram_messenger/SplashActivity.kt
@@ -0,0 +1,42 @@
+/**
+ * Created by Dmitry Popov on 15.09.2022.
+ */
+package io.fasthome.fenestram_messenger
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+
+    private companion object {
+        const val SPLASH_DELAY = 1000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            delay(SPLASH_DELAY)
+            openMain()
+        }
+    }
+
+    private fun openMain() {
+        val mainIntent = Intent(this, MainActivity::class.java)
+
+        if (!intent.wasLaunchedFromRecent) {
+            intent.action?.let(mainIntent::setAction)
+            intent.data?.let(mainIntent::setData)
+            intent.extras?.let(mainIntent::putExtras)
+        }
+
+        startActivity(mainIntent)
+    }
+
+    private val Intent.wasLaunchedFromRecent: Boolean
+        get() = (flags and Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) == Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
+
+}
