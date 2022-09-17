6f39b0fa2bbd855829cc1b5f79ef891f2efebf6f
RudakoVladimir <v.rudakov@fasthome.io>
2022-09-17 16:53:21 +0300

    GLOBAL-938 Добавление картинки для группового чата на CreateInfoFragment.kt

--- features/messenger/messenger-impl/src/main/java/io/fasthome/fenestram_messenger/messenger_impl/presentation/create_group_chat/create_info/CreateInfoFragment.kt
+++ features/messenger/messenger-impl/src/main/java/io/fasthome/fenestram_messenger/messenger_impl/presentation/create_group_chat/create_info/CreateInfoFragment.kt
@@ -5,21 +5,46 @@ package io.fasthome.fenestram_messenger.messenger_impl.presentation.create_group
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.children
+import io.fasthome.component.pick_file.PickFileComponentContract
+import io.fasthome.component.pick_file.PickFileComponentParams
+import io.fasthome.component.select_from.SelectFromDialog
 import io.fasthome.fenestram_messenger.messenger_impl.R
 import io.fasthome.fenestram_messenger.messenger_impl.databinding.FragmentCreateInfoChatBinding
 import io.fasthome.fenestram_messenger.messenger_impl.presentation.create_group_chat.select_participants.adapter.ContactsAdapter
 import io.fasthome.fenestram_messenger.mvi.BaseViewEvent
 import io.fasthome.fenestram_messenger.mvi.ViewModelInterface
 import io.fasthome.fenestram_messenger.presentation.base.ui.BaseFragment
+import io.fasthome.fenestram_messenger.presentation.base.ui.registerFragment
+import io.fasthome.fenestram_messenger.presentation.base.util.InterfaceFragmentRegistrator
 import io.fasthome.fenestram_messenger.presentation.base.util.fragmentViewBinding
 import io.fasthome.fenestram_messenger.presentation.base.util.noEventsExpected
 import io.fasthome.fenestram_messenger.presentation.base.util.viewModel
+import io.fasthome.fenestram_messenger.util.RoundedCornersOutlineProvider
+import io.fasthome.fenestram_messenger.util.dp
+import io.fasthome.fenestram_messenger.util.model.Bytes
 import io.fasthome.fenestram_messenger.util.onClick
 
-class CreateInfoFragment : BaseFragment<CreateInfoState, CreateInfoEvent>(R.layout.fragment_create_info_chat) {
+class CreateInfoFragment :
+    BaseFragment<CreateInfoState, CreateInfoEvent>(R.layout.fragment_create_info_chat) {
 
-    override val vm : CreateInfoViewModel by viewModel(
-        getParamsInterface = CreateInfoContract.getParams
+    private val pickImageFragment by registerFragment(
+        componentFragmentContractInterface = PickFileComponentContract,
+        paramsProvider = {
+            PickFileComponentParams(
+                mimeType = PickFileComponentParams.MimeType.Image(
+                    compressToSize = Bytes(
+                        Bytes.BYTES_PER_MB
+                    )
+                )
+            )
+        }
+    )
+
+    override val vm: CreateInfoViewModel by viewModel(
+        getParamsInterface = CreateInfoContract.getParams,
+        interfaceFragmentRegistrator = InterfaceFragmentRegistrator()
+            .register(::pickImageFragment)
     )
 
     private val binding by fragmentViewBinding(FragmentCreateInfoChatBinding::bind)
@@ -37,12 +62,37 @@ class CreateInfoFragment : BaseFragment<CreateInfoState, CreateInfoEvent>(R.layo
         next.onClick {
             vm.onReadyClicked(chatName.text.toString())
         }
+        chatAvatar.onClick {
+            vm.onAvatarClicked()
+        }
     }
 
     override fun renderState(state: CreateInfoState) {
         adapter.items = state.contacts
+        state.avatarImage?.let { avatarImage ->
+            with(binding.chatAvatar) {
+                clipToOutline = true
+                outlineProvider = RoundedCornersOutlineProvider(height.toFloat())
+                setImageBitmap(avatarImage.bitmap)
+            }
+        }
     }
 
-    override fun handleEvent(event: CreateInfoEvent) = noEventsExpected()
+    override fun handleEvent(event: CreateInfoEvent) {
+        when (event) {
+            is CreateInfoEvent.ShowSelectFromDialog -> {
+                SelectFromDialog
+                    .create(
+                        fragment = this,
+                        fromCameraClicked = {
+                            vm.selectFromCamera()
+                        },
+                        fromGalleryClicked = {
+                            vm.selectFromGallery()
+                        })
+                    .show()
+            }
+        }
+    }
 
 }
