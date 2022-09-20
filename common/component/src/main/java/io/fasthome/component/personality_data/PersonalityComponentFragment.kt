2b08fb90385665fdc4674cf0fad253d6ae7e272f
RudakoVladimir <v.rudakov@fasthome.io>
2022-09-20 17:00:05 +0300

    GLOBAL-1043 Исправлена валидация на экране заполнения личных данных

--- common/component/src/main/java/io/fasthome/component/personality_data/PersonalityComponentFragment.kt
+++ common/component/src/main/java/io/fasthome/component/personality_data/PersonalityComponentFragment.kt
@@ -27,7 +27,7 @@ class PersonalityComponentFragment :
     private val binding by fragmentViewBinding(FragmentPersonalityComponentBinding::bind)
 
     companion object {
-        const val MAX_SYMBOLS = 25
+        const val MAX_SYMBOLS = 30
     }
 
     override val vm: PersonalityComponentViewModel by viewModel(
@@ -58,24 +58,18 @@ class PersonalityComponentFragment :
         )
 
         nameInput.doAfterTextChanged {
-            if (!it.isNullOrEmpty()) {
-                vm.onFieldChanged(EditTextKey.UsernameKey, it.toString())
-            }
+            vm.onFieldChanged(EditTextKey.UsernameKey, it?.toString() ?: "")
         }
         mailInput.doAfterTextChanged {
-            if (!it.isNullOrEmpty()) {
-                vm.onFieldChanged(EditTextKey.MailKey, it.toString())
-            }
+            vm.onFieldChanged(EditTextKey.MailKey, it?.toString() ?: "")
+
         }
         userNameInput.doAfterTextChanged {
-            if (!it.isNullOrEmpty()) {
-                vm.onFieldChanged(EditTextKey.NicknameKey, it.toString())
-            }
+            vm.onFieldChanged(EditTextKey.NicknameKey, it?.toString() ?: "")
+
         }
         birthdateInput.doAfterTextChanged {
-            if (!it.isNullOrEmpty()) {
-                vm.onFieldChanged(EditTextKey.BirthdateKey, it.toString())
-            }
+            vm.onFieldChanged(EditTextKey.BirthdateKey, it?.toString() ?: "")
         }
         HooliDatePicker(birthdateInput).registerDatePicker(childFragmentManager)
     }
