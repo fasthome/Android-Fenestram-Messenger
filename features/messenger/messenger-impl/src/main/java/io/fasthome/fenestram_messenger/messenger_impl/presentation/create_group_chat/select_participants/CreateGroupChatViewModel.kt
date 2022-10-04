39c30f05bd75c2d492776f870aeb3ebce04a431c
Dmitry <dvm101@yandex.ru>
2022-10-04 17:44:22 +0300

    GLOBAL-1117 Реализовано создание приватного чата

--- features/messenger/messenger-impl/src/main/java/io/fasthome/fenestram_messenger/messenger_impl/presentation/create_group_chat/select_participants/CreateGroupChatViewModel.kt
+++ features/messenger/messenger-impl/src/main/java/io/fasthome/fenestram_messenger/messenger_impl/presentation/create_group_chat/select_participants/CreateGroupChatViewModel.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.viewModelScope
 import io.fasthome.fenestram_messenger.contacts_api.ContactsFeature
 import io.fasthome.fenestram_messenger.contacts_api.model.Contact
 import io.fasthome.fenestram_messenger.messenger_impl.R
+import io.fasthome.fenestram_messenger.messenger_impl.domain.entity.Chat
+import io.fasthome.fenestram_messenger.messenger_impl.presentation.conversation.ConversationNavigationContract
 import io.fasthome.fenestram_messenger.messenger_impl.presentation.create_group_chat.create_info.CreateInfoContract
 import io.fasthome.fenestram_messenger.messenger_impl.presentation.create_group_chat.select_participants.mapper.mapToContactViewItem
 import io.fasthome.fenestram_messenger.messenger_impl.presentation.create_group_chat.select_participants.model.ContactViewItem
@@ -20,11 +22,14 @@ import kotlinx.coroutines.launch
 class CreateGroupChatViewModel(
     requestParams: RequestParams,
     router: ContractRouter,
-    private val contactsFeature: ContactsFeature
+    private val contactsFeature: ContactsFeature,
+    private val params: CreateGroupChatContract.Params
 ) : BaseViewModel<CreateGroupChatState, CreateGroupChatEvent>(router, requestParams) {
 
     private val createInfoLauncher = registerScreen(CreateInfoContract)
     private var originalContacts = listOf<Contact>()
+
+    private val conversationLauncher = registerScreen(ConversationNavigationContract) { }
 
     init {
         viewModelScope.launch {
@@ -40,7 +45,7 @@ class CreateGroupChatViewModel(
     }
 
     override fun createInitialState(): CreateGroupChatState {
-        return CreateGroupChatState(listOf(), listOf(), false)
+        return CreateGroupChatState(listOf(), listOf(), false, params.isGroupChat)
     }
 
     fun onContactClicked(contactViewItem: ContactViewItem) {
@@ -48,7 +53,10 @@ class CreateGroupChatViewModel(
             val newList = if (contactViewItem.isSelected) {
                 state.addedContacts.filter { it.userId != contactViewItem.userId }
             } else {
-                state.addedContacts.plus(contactViewItem)
+                if (params.isGroupChat)
+                    state.addedContacts.plus(contactViewItem)
+                else
+                    state.addedContacts.dropLast(1).plus(contactViewItem)
             }
             state.copy(addedContacts = newList, needScroll = true)
         }
@@ -73,7 +81,25 @@ class CreateGroupChatViewModel(
                 viewItem.userId == contact.userId
             }
         }
-        createInfoLauncher.launch(CreateInfoContract.Params(contacts))
+
+        if (params.isGroupChat)
+            createInfoLauncher.launch(CreateInfoContract.Params(contacts))
+        else {
+            router.backTo(null)
+            conversationLauncher.launch(
+                ConversationNavigationContract.Params(
+                    chat = Chat(
+                        name = contacts[0].userName ?: "",
+                        users = listOf(contacts[0].user!!.id),
+                        isGroup = false,
+                        time = null,
+                        messages = listOf(),
+                        avatar = contacts[0].user!!.avatar,
+                        id = null,
+                    )
+                )
+            )
+        }
     }
 
     override fun onBackPressed(): Boolean {
