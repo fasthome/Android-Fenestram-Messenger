2d2043dccda43632be0513a3be8612454bb494da
MrMilkot <95224952+DKartovitskii@users.noreply.github.com>
2022-10-13 11:01:53 +0300

    GLOBAL-1135 фикс анимации

--- common/component/src/main/java/io/fasthome/component/imageViewer/ImageViewerFragment.kt
+++ common/component/src/main/java/io/fasthome/component/imageViewer/ImageViewerFragment.kt
@@ -4,26 +4,30 @@ import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import io.fasthome.component.R
-import io.fasthome.fenestram_messenger.core.ui.extensions.loadRounded
 import io.fasthome.component.databinding.FragmentImageViewerBinding
+import io.fasthome.fenestram_messenger.core.ui.extensions.loadRounded
 import io.fasthome.fenestram_messenger.presentation.base.ui.BaseFragment
 import io.fasthome.fenestram_messenger.presentation.base.util.fragmentViewBinding
 import io.fasthome.fenestram_messenger.presentation.base.util.noEventsExpected
 import io.fasthome.fenestram_messenger.presentation.base.util.viewModel
 import io.fasthome.fenestram_messenger.util.onClick
 
-class ImageViewerFragment : BaseFragment<ImageViewerState, ImageViewerEvent>(R.layout.fragment_image_viewer) {
+class ImageViewerFragment :
+    BaseFragment<ImageViewerState, ImageViewerEvent>(R.layout.fragment_image_viewer) {
 
     private val binding by fragmentViewBinding(FragmentImageViewerBinding::bind)
 
-    override val vm : ImageViewerViewModel by viewModel(
+    override val vm: ImageViewerViewModel by viewModel(
         getParamsInterface = ImageViewerContract.getParams
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ibCancel.onClick(vm::onBackPressed)
-        binding.image.setOnSwipeDownListener(vm::onBackPressed)
+        binding.image.setOnSwipeDownListener {
+            binding.root.animate().alpha(0f).setDuration(300).start()
+            vm.onBackPressed()
+        }
         binding.image.setOnAlphaChangedListener { alpha ->
             binding.root.alpha = alpha
         }
@@ -35,7 +39,7 @@ class ImageViewerFragment : BaseFragment<ImageViewerState, ImageViewerEvent>(R.l
                 loadRounded(state.imageBitmap, radius = 1, transform = FitCenter())
             }
             state.imageUrl?.let {
-                loadRounded(state.imageUrl, radius = 1,  transform = FitCenter())
+                loadRounded(state.imageUrl, radius = 1, transform = FitCenter())
             }
         }
     }
