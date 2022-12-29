7b53e7d042b59e11cbe16b75a9596214cfa2ba3e
MrMilkot <95224952+DKartovitskii@users.noreply.github.com>
2022-12-29 15:25:12 +0300

    GLOBAL-1794 space name surname regex

--- common/util/src/main/java/io/fasthome/fenestram_messenger/util/model/EditTextFilter.kt
+++ common/util/src/main/java/io/fasthome/fenestram_messenger/util/model/EditTextFilter.kt
@@ -2,8 +2,9 @@ package io.fasthome.fenestram_messenger.util.model
 
 import android.text.InputFilter
 import android.text.Spanned
+import io.fasthome.fenestram_messenger.util.REGEX_EDIT_TEXT
 
-class EditTextFilter : InputFilter {
+class EditTextFilter(private val patten: Regex = REGEX_EDIT_TEXT) : InputFilter {
     override fun filter(
         source: CharSequence?,
         start: Int,
@@ -12,7 +13,7 @@ class EditTextFilter : InputFilter {
         dstart: Int,
         dend: Int
     ): CharSequence? {
-        return if (source.toString().matches(Regex("[a-zA-Zа-яА-Я]+")))
+        return if (source.toString().matches(patten))
             source
         else
             source?.dropLast(1)
